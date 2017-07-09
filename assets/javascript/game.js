b99faf3fe170175aee11ffd8ac6b7ac2eaae7e86
Philippe <philippesdixon@gmail.com>
2017-07-09 19:00:32 -0400

    added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 var liChild;	//will generate <li> element for wrong guesses
+var audioElement;
 
 hangman = {
 	startText : "Let the games begin!!! Guess a letter:",
@@ -13,28 +14,42 @@ hangman = {
 	blank : "<li>__ &nbsp;</li>",
 	wrongLetters : "",
 	userInput : "",
-	wordSelect: function() {
+	imagePath : "",
+	imageSource : "assets/images/hangman",
+	initialImage : "assets/images/hangman0.png",
+	wordSelect: function() {	//randomly selects for from wordBank
 		var random = Math.floor(Math.random() * 10);
 		var word = this.wordBank[random];
 		console.log(word);
 		return word
 	},
 	wordGenerate: function () {
-		this.word = this.wordSelect();
+		this.word = this.wordSelect();	//generate word for user to guess
 		this.removeCorrectGuesses = this.word;
-		//generate correct number of blank slots 
-		for (var i = 1; i < this.word.length; i++) {
+		for (var i = 1; i < this.word.length; i++) {	//generate correct number of blank slots
 			hangman.blank += "<li>__ &nbsp;</li>";
 		}		
-		document.getElementById('blanks').innerHTML = hangman.blank;
+		document.getElementById('blanks').innerHTML = hangman.blank; //write blanks to document
 	},
-	newGame : function () {
+	newGame : function () {		//reset values for new game and write them to the document
 		this.guesses = 8;
 		this.wrongLetters = "";
 		this.blank = "<li>__ &nbsp;</li>";
 		document.getElementById('wrong').innerHTML = this.wrongLetters;
 		document.getElementById('guesses').innerHTML =		
-		'<p>Guesses: ' + this.guesses + '</p>';	}		
+		'<p>Guesses: ' + this.guesses + '</p>';	
+		document.getElementById('image').setAttribute('src', 'assets/images/hangman8.png')
+	},
+	winSound : function() {
+		audioElement.setAttribute('src', './assets/audio/slots.mp3');
+  		audioElement.play();
+	},
+	playAgain : function () {
+		setTimeout(function(){ 
+						hangman.newGame();
+						hangman.wordGenerate();
+						alert('click to play again');
+					}, 50)}	
 }
 document.onkeyup = function(event) {
 	if(event.key === 'Enter'){
@@ -57,8 +72,11 @@ document.onkeyup = function(event) {
 				if (hangman.removeCorrectGuesses.length === 0) {	//update wins and reset game
 					hangman.wins++;
 					document.getElementById('wins').innerHTML = "Wins: " + hangman.wins;
-					hangman.newGame();
-					hangman.wordGenerate();
+					// hangman.newGame();
+					// hangman.wordGenerate();
+					audioElement = document.createElement('audio');
+  					hangman.winSound();
+					hangman.playAgain();
 				}
 			} else {	// if no match, substract guesses, write bad letter and remaining guesses to document
 				if (hangman.wrongLetters.indexOf(hangman.userInput) === -1) {	// check if letter previously guessed
@@ -70,19 +88,19 @@ document.onkeyup = function(event) {
 					document.getElementById('wrong').appendChild(liChild);
 					document.getElementById('wrong').children[0].innerHTML =
 						'<li>' + hangman.wrongLetters.toUpperCase() + '</li>';
+
+					imagePath = hangman.imageSource + String(hangman.guesses) + ".png";					
+					document.getElementById('image').setAttribute('src', imagePath);
 				}
 				if (hangman.guesses === 0) {	//update losses and reset game
 					hangman.losses++;
 					document.getElementById('losses').innerHTML = "Losses: " + hangman.losses;
-					hangman.newGame();
-					hangman.wordGenerate();
+					hangman.playAgain();
 				}
 			}							
 		}	
 	}
 }
-
-// add music file for winning
 // add stick figure pics
 
 
